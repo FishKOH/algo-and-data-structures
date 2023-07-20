c6f120234cb5fe514a283ba8d444126ed58f0e37
Nesterenko Sergey <nestersv8@gmail.com>
2023-07-20 13:02:05 +0700

    add realization/tests for insert, delete

--- 3_dynamic_array/dynamic_array.py
+++ 3_dynamic_array/dynamic_array.py
@@ -2,13 +2,29 @@ import ctypes
 
 class DynArray:
     
+    realloc_multiplier = 2
+    dealloc_threshold = 0.5
+    dealloc_divider = 1.5
+    minimal_capacity = 16
+    
     def __init__(self):
         self.count = 0
-        self.capacity = 16
+        self.capacity = self.minimal_capacity
         self.array = self.make_array(self.capacity)
 
     def __len__(self):
         return self.count
+
+    def __fullness(self):
+        return self.count/self.capacity
+
+    def __realloc(self):
+        if self.count == self.capacity:
+            self.resize(self.realloc_multiplier*self.capacity)
+
+    def __dealloc(self):
+        if self.__fullness() < self.dealloc_threshold:
+            self.resize(max(self.minimal_capacity, int(self.capacity / self.dealloc_divider)))
 
     def make_array(self, new_capacity):
         return (new_capacity * ctypes.py_object)()
@@ -19,7 +35,7 @@ class DynArray:
         return self.array[i]
 
     def resize(self, new_capacity):
-        assert new_capacity >= 16, f"Couldn't resize({new_capacity}). 16 is minimal capacity"
+        assert new_capacity >= self.minimal_capacity, f"Couldn't resize({new_capacity}). {self.minimal_capacity} is minimal capacity"
         
         new_array = self.make_array(new_capacity)
         for i in range(self.count):
@@ -28,15 +44,24 @@ class DynArray:
         self.capacity = new_capacity
 
     def append(self, itm):
-        if self.count == self.capacity:
-            self.resize(2*self.capacity)
+        self.__realloc()
         self.array[self.count] = itm
         self.count += 1
 
     def insert(self, i, itm):
-        # добавляем объект itm в позицию i, начиная с 0
-        pass
+        if i < 0 or i > self.count:
+            raise IndexError('Index is out of bounds')
+        self.__realloc()
+        for idx in reversed(range(i, self.count)):
+            self.array[idx+1] = self.array[idx]
+        self.array[i] = itm
+        self.count += 1
 
     def delete(self, i):
-        pass
-        # удаляем объект в позиции i
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        for i in range(i, self.count-1):
+            self.array[i] = self.array[i+1]
+        self.count -= 1    
+        self.__dealloc()
+
