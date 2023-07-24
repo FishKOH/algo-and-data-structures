21bc49c5efe080fc7d21147084b5a9baf812f01c
Nesterenko Sergey <nestersv8@gmail.com>
2023-07-25 01:16:37 +0700

    add realization stack:
    - v0: push to tail
    - v1: push to head

--- 4_stack/stack.py
+++ 4_stack/stack.py
@@ -1,7 +1,71 @@
+class BaseNode:
+    def __init__(self):
+        self.prev = None
+        self.next = None
+
+class Node(BaseNode):
+    def __init__(self, v):
+        self.value = v
+        super().__init__()
+
+class LinkedList2:  
+    def __init__(self):
+        self.__dummy_head = BaseNode()
+        self.__dummy_head.next = self.__dummy_head
+        self.__dummy_head.prev = self.__dummy_head
+
+    def empty(self):
+        return self.__dummy_head.next == self.__dummy_head    
+
+    def head(self):
+        return None if self.empty() else self.__dummy_head.next
+
+    def tail(self):
+        return None if self.empty() else self.__dummy_head.prev
+
+    def __insert(self, after_node, new_node):
+        new_node.next = after_node.next
+        new_node.prev = after_node
+
+        after_node.next.prev = new_node
+        after_node.next = new_node
+
+    def __delete(self, delete_node):
+        delete_node.prev.next = delete_node.next
+        delete_node.next.prev = delete_node.prev
+
+    def add_in_tail(self, item):
+        self.__insert(self.__dummy_head.prev, item)
+
+    def len(self):
+        node_count = 0
+        node = self.head()
+        while isinstance(node, Node):
+            node_count += 1
+            node = node.next
+        return node_count
+
+    __len__ = len
+
+    def add_in_head(self, newNode):
+        if self.empty():
+            self.add_in_tail(newNode)
+        else:
+            self.__insert(self.__dummy_head, newNode)
+    
+    def delete_in_tail(self):
+        if not self.empty():
+            self.__delete(self.__dummy_head.prev)
+
+    def delete_in_head(self):
+        if not self.empty():
+            self.__delete(self.__dummy_head.next)
+
 class Stack:
     def __init__(self):
-        self.stack = [] 
-        # |      | native_list/dynamic_array      | linked_list                   |
+        self.stack = LinkedList2() 
+        # Time Complexity
+        # |      | native_list/dynamic_array      | doubly linked_list            |
         # | ---  | ---                            | ---                           |
         # | tail | size O(1) pop O(1) push O(1)   | size O(N) pop O(1) push O(1)  |
         # | head | size O(1) pop O(N) push O(N)   | size O(N) pop O(1) push O(1)  |
@@ -10,13 +74,43 @@ class Stack:
         return len(self.stack)
 
     def pop(self):
-        # ваш код
-        return None # если стек пустой
+        if self.stack.head() is None:
+            return None
+        else:
+            saved_value = self.peek()
+            self.stack.delete_in_head()
+            return saved_value
 
     def push(self, value):
-        # ваш код
-        pass
+        self.stack.add_in_head(Node(value))
 
     def peek(self):
-        # ваш код
-        return None # если стек пустой
+        if self.stack.head() is None:
+            return None
+        else:
+            return self.stack.head().value
+
+
+class Stack_v0:
+    def __init__(self):
+        self.stack = LinkedList2() 
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        if self.stack.tail() is None:
+            return None
+        else:
+            saved_value = self.peek()
+            self.stack.delete_in_tail()
+            return saved_value
+
+    def push(self, value):
+        self.stack.add_in_tail(Node(value))     
+
+    def peek(self):
+        if self.stack.tail() is None:
+            return None
+        else:
+            return self.stack.tail().value
