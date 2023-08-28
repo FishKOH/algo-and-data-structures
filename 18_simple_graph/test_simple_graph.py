95d4a637bd2a4956d7a184a88d58b2de75c232ae
Nesterenko Sergey <nestersv8@gmail.com>
2023-08-29 00:46:42 +0700

    add tests/realization WeakVerticies

--- 18_simple_graph/test_simple_graph.py
+++ 18_simple_graph/test_simple_graph.py
@@ -189,3 +189,62 @@ class TestBfs(unittest.TestCase):
         self.assertEqual(graph.BreadthFirstSearch(0,4), [graph.vertex[0], graph.vertex[1], graph.vertex[2], graph.vertex[3], graph.vertex[4]])
         graph.RemoveEdge(0, 1)
         self.assertEqual(graph.BreadthFirstSearch(0,4), [])
+        
+class TestWeakVertices(unittest.TestCase):
+
+    def test_one_triangle(self):
+        graph = SimpleGraph(3)
+        for i in range(3):
+            graph.AddVertex(i)
+        graph.AddEdge(0,1)
+        graph.AddEdge(0,2)
+        graph.AddEdge(1,2)
+        
+        self.assertEqual(graph.WeakVertices(), [])
+    
+    def test_one_triangle_one_vertex(self):
+        graph = SimpleGraph(4)
+        for i in range(4):
+            graph.AddVertex(i)
+        graph.AddEdge(0,1)
+        graph.AddEdge(0,2)
+        graph.AddEdge(1,2)
+        graph.AddEdge(2,3)
+        
+        self.assertEqual(graph.WeakVertices(), [graph.vertex[3]])
+    
+    def test_two_triangle(self):
+        graph = SimpleGraph(6)
+        for i in range(6):
+            graph.AddVertex(i)
+        graph.AddEdge(0,1)
+        graph.AddEdge(0,2)
+        graph.AddEdge(1,2)
+        graph.AddEdge(3,4)
+        graph.AddEdge(3,5)
+        graph.AddEdge(4,5)
+        
+        self.assertEqual(graph.WeakVertices(), [])
+    
+    def test_complex(self):
+        graph = SimpleGraph(9)
+        for i in range(9):
+            graph.AddVertex(i)
+        graph.AddEdge(0,1)
+        graph.AddEdge(0,2)
+        graph.AddEdge(1,2)
+        graph.AddEdge(1,3)
+        graph.AddEdge(2,3)
+        
+        graph.AddEdge(4,5)
+        graph.AddEdge(4,6)
+        graph.AddEdge(5,6)
+        
+        graph.AddEdge(2,4)
+        
+        graph.AddEdge(0,7)
+        graph.AddEdge(4,7)
+        graph.AddEdge(6,8)
+        
+        self.assertEqual(graph.WeakVertices(), [graph.vertex[7], graph.vertex[8]])
+
